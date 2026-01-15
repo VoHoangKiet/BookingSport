4edc2e72d6e147bb7eb39ef01ac6d5234982d9af
ngochao2503 <ngcha763@gmail.com>
2026-01-15 23:42:31 +0700

    Fix: google auth

--- src/admin/src/pages/AuthSuccess.tsx
+++ src/admin/src/pages/AuthSuccess.tsx
@@ -3,11 +3,13 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import { saveToken } from "../services/auth";
 import { decodeToken } from "../utils/jwt";
 import { useAuthStore } from "@/stores/auth.store";
+import { userApi } from "@/api";
+import { User } from "@/types";
 
 export default function AuthSuccess() {
   const [params] = useSearchParams();
   const navigate = useNavigate();
-  const { setToken } = useAuthStore()
+  const { setToken, setUser } = useAuthStore()
 
   useEffect(() => {
     const token = params.get("token");
@@ -17,9 +19,22 @@ export default function AuthSuccess() {
       navigate("/login");
       return;
     }
+
+    const getUserInfo = async () => {
+      try {
+        const user: User & { ten: string } = await userApi.getProfile() as any
+        if (user) {
+          setUser({ ...user, ho_ten: user.ten })
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
     try {
       saveToken(token);
       setToken(token)
+      getUserInfo()
 
       const payload = decodeToken(token);
 
