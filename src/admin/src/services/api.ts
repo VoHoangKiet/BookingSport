04eeb4fed5d8ea879de729ba967723d63a864084
VoHoangKiet <muoivo.051064@gmail.com>
2026-01-16 09:41:36 +0700

    fix:add prefix

--- src/admin/src/services/api.ts
+++ src/admin/src/services/api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:3000/api",
-  baseURL: API_BASE_URL,
+  baseURL: API_BASE_URL + "/api",
   headers: {
     'Content-Type': 'application/json',
   },
